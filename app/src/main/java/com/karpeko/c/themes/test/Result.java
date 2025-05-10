562ee48a2f49630627af9c6c6df86b0a897731ba
FuchsJinx <153663768+FuchsJinx@users.noreply.github.com>
2025-05-10 19:49:59 +0300

    Add animation in result fragment

--- app/src/main/java/com/karpeko/c/themes/test/Result.java
+++ app/src/main/java/com/karpeko/c/themes/test/Result.java
@@ -1,5 +1,6 @@
 package com.karpeko.c.themes.test;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -12,16 +13,23 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.karpeko.c.R;
+import com.karpeko.c.notification.SoundClick;
 
 public class Result extends Quest {
 
     TextView title, text;
 
+    LottieAnimationView animationView;
+
+    @SuppressLint("MissingInflatedId")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.result, container, false);
+
+        animationView = view.findViewById(R.id.lottie_animation);
 
         title = view.findViewById(R.id.title);
         text = view.findViewById(R.id.text);
@@ -29,28 +37,34 @@ public class Result extends Quest {
         int result = CountingResults.isRightAnswer;
         if (result == 20) {
             title.setText("Вы идеально прошли тест! 5+");
+            animationView.setAnimation(R.raw.finish_animation);
             CountingResults.isRightAnswer = 0;
         }
         else if (result > 17) {
             title.setText("Вы прекрасно справились! 5");
+            animationView.setAnimation(R.raw.five);
             CountingResults.isRightAnswer = 0;
         }
         else if (result > 14) {
             title.setText("Вы хорошо справились! 4");
+            animationView.setAnimation(R.raw.four);
             CountingResults.isRightAnswer = 0;
         }
         else if (result > 10) {
             title.setText("Вы справились удовлетворительно! 3");
+            animationView.setAnimation(R.raw.three);
             CountingResults.isRightAnswer = 0;
         }
         else {
             title.setText("Вы завалили экзамен! На пересдачу");
+            animationView.setAnimation(R.raw.two);
             CountingResults.isRightAnswer = 0;
             text.setVisibility(View.INVISIBLE);
         }
 
 
         text.setText(result + " из " + 20 + " вопросов правильно!");
+        animationView.playAnimation();
 
         onClick(view,22);
 
@@ -60,6 +74,7 @@ public class Result extends Quest {
     protected void onClick(View view, int index) {
         Button button = view.findViewById(R.id.button);
         button.setOnClickListener(v -> {
+            SoundClick.soundClick(getContext());
             shared(index);
             getActivity().finish();
         });
